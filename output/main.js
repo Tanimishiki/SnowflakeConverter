dd86f8c92675e2f6e2d872ec93633e0d19acbce8
TankArgie <60542682+TankArgie@users.noreply.github.com>
2021-08-18 19:00:44 +0800

    Tank

--- output/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const snowflakeConverter_1 = require("./snowflakeConverter");
-const userId = "419343276265439234"; // TankArgie user ID.
-const date = snowflakeConverter_1.SnowflakeConverter.convert(userId); // Returns object.
-console.log(date);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { SnowflakeConverter } from "./snowflakeConverter";
 
-const userId = "419343276265439234"; // TankArgie user ID.
+const userId = "617194157903183928"; // TankArgie user ID.
 const date = SnowflakeConverter.convert(userId); // Returns object.
 
 console.log(date);
